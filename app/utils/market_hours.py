a3c16953e5da7d3178e2d1ce4a409045dacd93ec
Luigi Burga <luigiburga@hotmail.com>
2026-05-29 18:53:36 -0500

    new feature

--- app/utils/market_hours.py
+++ app/utils/market_hours.py
@@ -8,14 +8,14 @@ def market_is_open():
     now = datetime.now(est)
 
     market_open = now.replace(
-        hour=9,  #  9
-        minute=15, # 30
+        hour=0,  #  9
+        minute=0, # 30
         second=0
     )
 
     market_close = now.replace(
-        hour=15,   # 16
-        minute=50,  #00 
+        hour=0,   # 16
+        minute=0,  #00 
         second=0
     )
 
